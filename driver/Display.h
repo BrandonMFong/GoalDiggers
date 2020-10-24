c1884e2d26d65f9341d9261f244aae3b7bdbf674
BrandonMFong <fong.m.brandon97@gmail.com>
2020-10-23 19:53:36 -0700

    setting the D pins as output on the node

--- driver/Display.h
+++ driver/Display.h
@@ -7,13 +7,17 @@ void init_7SegmentDisplay();
 void SetSevenSegmentDisplay(int value);
 void Display();
 void AssignSeg(uint8_t val);
+void GetOutputValue(uint8_t val);
 
 /* IMPORTANT VARIALBLES START */
+int test_AQI = 0; // temp
 int CurrentState = 0;
 const uint8_t AQIDisplay = 0;
 const uint8_t TempDisplay = 1;
-int ledPin = 12;
-int test_AQI = 0;
+int pinD1 = 10; // GPIO10
+int pinD2 = 16; // GPIO16
+int pinD3 = 5; // GPIO5
+int pinD4 = 4; // GPIO4
 
 volatile uint8_t ssdReg;
 
@@ -30,7 +34,12 @@ volatile uint8_t ssdReg;
 // INIT
 void init_7SegmentDisplay()
 {
-  pinMode(ledPin, OUTPUT);
+  // init segment output pin 
+  pinMode(pinD1, OUTPUT);
+  pinMode(pinD2, OUTPUT);
+  pinMode(pinD3, OUTPUT);
+  pinMode(pinD4, OUTPUT);
+  
   SSDState = 0x00; // init the value of the state 
 }
 
@@ -52,8 +61,23 @@ void SetSevenSegmentDisplay(int value)
   else ssdReg = ssdReg << 1;
 }
 
+// do I need to invert the bits? 
 void AssignSeg(uint8_t val)
 {
-  // do I need to invert the bits? 
-  
+  // shift the bits to index 0 and AND it to get the value for the pin
+  // bits -> xxxx <pinD4><pinD3><pinD2><pin1>
+  uint8_t var0 = (val >> 0) & 0x01;
+  digitalWrite(pinD1, GetOutputValue(var0));   // D1
+  uint8_t var1 = (val >> 1) & 0x01;
+  digitalWrite(pinD2, GetOutputValue(var1));   // D2
+  uint8_t var2 = (val >> 2) & 0x01;
+  digitalWrite(pinD3, GetOutputValue(var2));   // D3
+  uint8_t var3 = (val >> 3) & 0x01;
+  digitalWrite(pinD4, GetOutputValue(var3));   // D4
+}
+
+// translates value to the arduino board digital values
+uint8_t GetOutputValue(uint8_t val)
+{
+  return (val == 0) ? LOW : HIGH;
 }
