ea948e4cbf136aaa573c7cb51d8a83aa7b5a59cb
BrandonMFong <fong.m.brandon97@gmail.com>
2020-10-23 21:09:46 -0700

    setting up the segment value

--- driver/Display.h
+++ driver/Display.h
@@ -10,12 +10,15 @@ void AssignSeg(uint8_t val);
 void AssignVal(uint8_t val);
 void GetOutputValue(uint8_t val);
 void ExtractSegmentValues(int val,int index);
+void SetSegmentValue(int val);
 
 /* IMPORTANT VARIALBLES START */
-int test_AQI = 0; // temp
-int CurrentState = 0;
+volatile int AQI;
+volatile double TempF; // Fahrenheit
+
 const uint8_t AQIDisplay = 0;
 const uint8_t TempDisplay = 1;
+volatile int CurrentState = AQIDisplay; // test value
 
 // Segment control 
 int pinD1 = 10; // GPIO10
@@ -58,8 +61,8 @@ void Display()
 {
   switch(CurrentState)
   {
-    case AQIDisplay: SetSevenSegmentDisplay(test_AQI);
-    case TempDisplay: SetSevenSegmentDisplay(test_AQI);
+    case AQIDisplay: SetSevenSegmentDisplay(100); // test value
+    case TempDisplay: SetSevenSegmentDisplay(100);
   }
 }
 
@@ -67,7 +70,6 @@ void Display()
 void SetSevenSegmentDisplay(int value)
 {
   AssignSeg(); // turn the segment on 
-
   AssignVal(value);
 }
 
@@ -98,13 +100,13 @@ uint8_t GetOutputValue(uint8_t val)
   return (val == 0) ? LOW : HIGH;
 }
 
-int SegmentValArr [4] = {0,0,0,0};
+int SegmentValArr [4] = {0,0,0,0}; // array 
 // say a number is 4 digits long, like WXYZ 
 // WXYZ % 10 => Z
 // WXYZ * 0.1 => WXY.Z
 // int(WXY.Z) => WXY
 // then repeat
-void AssignVal(uint8_t val, )
+void AssignVal(uint8_t val)
 {
   // modulo 10
   ExtractSegmentValues(val,0); // separates WXYZ into {Z,Y,X,W}
@@ -114,16 +116,24 @@ void AssignVal(uint8_t val, )
   // 1000 = 8, log2(8) = 3 
   // therefore SegmentValArr[3] will give me the digit for the 4 segment (the left most segment)
   int index = log(ssdReg)/log(2); // Get the log base 2
-  
+
+  // Set value 
+  SetSegmentValue(SegmentValArr[index]);
 }
 
 // assume val is int
 // result should be in SegmentValArr array 
-ExtractSegmentValues(int val,int index)
+void ExtractSegmentValues(int val,int index)
 {
   int temp = val % 10;
   SegmentValArr[index] = temp;
 
   // int(val*0.1) shifts the number to the right
   if(index < 3) ExtractSegmentValues(int(val*0.1),index+1); // recurse into function if we still have some of the number left 
+}
+
+
+void SetSegmentValue(int val)
+{
+  
 }
